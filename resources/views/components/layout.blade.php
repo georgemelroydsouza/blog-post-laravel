3bb91666c74663928fe75a0bdfc626466636f0a0
George Melroy <george@buddi.co.uk>
2024-04-05 10:57:07 +0100

    Updated layout to split the navigation into the nav-link.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,8 +18,8 @@
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/" class="{{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Blog</a>
-                <a href="/about" class="{{ request()->is('about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium" aria-current="page">About</a>
+                <x-nav-link href="/" :active="request()->is('/')">Blog</x-nav-link>
+                <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
               </div>
             </div>
           </div>
