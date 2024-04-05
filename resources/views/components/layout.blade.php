9a29401a75f62366dd41ea4ab189698c2a708d6b
George Melroy <george@buddi.co.uk>
2024-04-05 11:37:47 +0100

    style: updated the default header and mobile header to use the nav-link.blade

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,8 +18,8 @@
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <x-nav-link href="/" :active="request()->is('/')">Blog</x-nav-link>
-                <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                <x-nav-link href="/" :active="request()->is('/')" nav="header">Blog</x-nav-link>
+                <x-nav-link href="/about" :active="request()->is('about')" nav="header">About</x-nav-link>
               </div>
             </div>
           </div>
@@ -60,8 +60,8 @@
       <div class="md:hidden" id="mobile-menu">
         <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
           <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-          <a href="/" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Blog</a>
-          <a href="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About</a>
+          <x-nav-link href="/" :active="request()->is('/')" nav="footer">Blog</x-nav-link>
+          <x-nav-link href="/about" :active="request()->is('/about')" nav="footer">About</x-nav-link>
         </div>
         <div class="border-t border-gray-700 pb-3 pt-4">
           <div class="flex items-center px-5">
